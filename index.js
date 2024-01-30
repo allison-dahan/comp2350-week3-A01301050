cd9ac508912ab46d070561734b13abf7a8e97066
Allison Dahan <allison.dahan@yahoo.ca>
2024-01-30 15:16:12 -0800

    change user info

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const port = process.env.PORT || 3000;
 const is_qoddi = process.env.IS_QODDI || false;
 
 const dbConfig = {
-	host: "host",
-	user: "user",
-	password: "password",
-	database: "database",
+	host: "sql.freedb.tech",
+	user: "freedb_2351_main",
+	password: "qE%6DmaDpeKvTpj",
+	database: "freedb_comp2350-week2-A01301050",
 	multipleStatements: false
 };
 
